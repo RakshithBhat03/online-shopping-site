36d21486f4348a1ba3e4202e88b9ce360cda53c1
RakshithBhat03 <rakshith6666@gmail.com>
2023-03-26 15:30:38 +0530

    [feat]: Add constants

---
+++ src/constants/constants.js
@@ -0,0 +1,9 @@
+export const INITIAL_PRODUCT_STATE = {
+  listing: [],
+  cart: [],
+  searchQuery: "",
+};
+export const ADD_TO_CART = "ADD_TO_CART";
+export const UPDATE_CART_QUANTITY = "UPDATE_CART_QUANTITY";
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
+export const UPDATE_SEARCH_QUERY = "UPDATE_SEARCH_QUERY";
