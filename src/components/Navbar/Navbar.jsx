8fb5d0da220020affba43abbab2307a09d503e40
RakshithBhat03 <rakshith6666@gmail.com>
2023-03-26 15:30:19 +0530

    [feat]: Routing and navigation

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,33 @@
+import { Link } from "react-router-dom";
+import { UPDATE_SEARCH_QUERY } from "../../constants/constants";
+import { useProduct } from "../../context/product-context";
 export const Navbar = () => {
+  const {
+    product: { searchQuery },
+    dispatch,
+  } = useProduct();
+  const handleChange = (e) =>
+    dispatch({ type: UPDATE_SEARCH_QUERY, payload: e.target.value });
   return (
     <nav>
-      <div className="flex items-center justify-between px-8 py-4 bg-blue-400">
-        <span>Home</span>
+      <div className="flex items-center justify-between px-8 py-4 bg-blue-600">
+        <Link to="/" className="text-white font-semibold text-xl md:text-2xl">
+          Home
+        </Link>
         <div className="w-[50%] ">
-          <input type="text" className="w-full border pl-1 rounded-sm" />
+          <input
+            value={searchQuery}
+            onChange={handleChange}
+            type="text"
+            className="w-full border pl-1 rounded-sm focus:outline-none py-1"
+            placeholder="Search Products"
+          />
         </div>
-        <div>Cart</div>
+        <Link
+          to="/cart"
+          className="text-white font-semibold text-xl md:text-2xl">
+          Cart
+        </Link>
       </div>
     </nav>
   );
--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-import { Cart, ProductListing } from "../pages/";
+import { Cart, ProductListing, SingleProduct } from "../pages/";
 export const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<ProductListing />}></Route>
       <Route path="/cart" element={<Cart />}></Route>
+      <Route path="/product/:id" element={<SingleProduct />}></Route>
     </Routes>
   );
 };
